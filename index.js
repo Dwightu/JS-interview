0bc4f50b92d0f302e8cbec525027e7d397828184
Dehui Yu <dyu105@uottawa.ca>
2022-08-09 13:43:42 -0400

    Update

--- index.js
+++ index.js
@@ -83,3 +83,58 @@ function outer() {
 // The ability of a function to store a variable for further reference even after it's executed is called Closure;
 
 
+// 11. What are object prototypes?
+// Like array prototype
+
+let arr=[];
+arr.push(2);
+console.log(arr);
+// The javascript engine sees that the method push does not exist on the current array object
+// and therefore, looks for the method push inside the Array prototype and it finds the method.
+
+
+// 12. Callback functions?
+// A callback function is a function that will be executed after another function is executed.
+
+
+// 13. var vs let
+var variable1=23;
+let variable2=89;
+
+window.variable2;// underfinded
+//the variables declared with the var keyword in the global scope are not added to the window/global object
+
+// 14. A block means the code written inside the curly braces{}
+// variables declared with var won;t affect by scope
+
+
+
+// 15. Spread operator.
+let fourNums=[1,2,3,4]
+
+function addFourNum(num1,num2,num3,num4){
+  return num1+num2+num3+num4;
+}
+
+addFourNum(...fourNums);
+
+// 16. Promise
+// four states:
+// 1. pending - initial state of promise.
+// 2. Fulfilled.
+// 3. Rejected.
+// 4. Settled. 
+function sumOfThreeNums(...elements){
+  return new Promise((resolve,reject)=>{
+    if(elements.length>3){
+      reject("we only support 3 elements!!!");
+    }else{
+      let sum=0;
+      elements.forEach(el=>{
+        sum+=el;
+      })
+    }
+    resolve("Sum has been calculated!!!!");
+    
+  })
+}
